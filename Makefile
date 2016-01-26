a6fa4c315461e778f067ef2d4e559d4101832454
Daniele Lacamera <root@danielinux.net>
2016-01-26 22:42:23 +0100

    apps.ld is generated at compile time + added string module

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+-include ../kconfig/.config
+-include ../config.mk
+
 CROSS_COMPILE?=arm-none-eabi-
 CC=$(CROSS_COMPILE)gcc
 CFLAGS+=-DCONFIG_FRESH=1
@@ -24,6 +27,23 @@ all: $(FROSTED)/apps.img
 $(FROSTED)/apps.img: apps.elf
 	$(CROSS_COMPILE)objcopy -O binary --pad-to=$$PADTO $^ $@
 
+apps/apps.ld: apps/apps.ld.in
+	export KMEM_SIZE_B=`python2 -c "print '0x%X' % ( $(KFLASHMEM_SIZE) * 1024)"`;	\
+	export AMEM_SIZE_B=`python2 -c "print '0x%X' % ( ($(RAM_SIZE) - $(KRAMMEM_SIZE)) * 1024)"`;	\
+	export KFLASHMEM_SIZE_B=`python2 -c "print '0x%X' % ( $(KFLASHMEM_SIZE) * 1024)"`;	\
+	export AFLASHMEM_SIZE_B=`python2 -c "print '0x%X' % ( ($(FLASH_SIZE) - $(KFLASHMEM_SIZE)) * 1024)"`;	\
+	export KRAMMEM_SIZE_B=`python2 -c "print '0x%X' % ( $(KRAMMEM_SIZE) * 1024)"`;	\
+	cat $^ | sed -e "s/__FLASH_ORIGIN/$(FLASH_ORIGIN)/g" | \
+			 sed -e "s/__KFLASHMEM_SIZE/$$KFLASHMEM_SIZE_B/g" | \
+			 sed -e "s/__AFLASHMEM_SIZE/$$AFLASHMEM_SIZE_B/g" | \
+			 sed -e "s/__RAM_BASE/$(RAM_BASE)/g" |\
+			 sed -e "s/__KRAMMEM_SIZE/$$KRAMMEM_SIZE_B/g" |\
+			 sed -e "s/__AMEM_SIZE/$$AMEM_SIZE_B/g" \
+			 >$@
+
+
+
+
 apps.elf: libfrosted.a $(APPS-y) apps/apps.ld
 	$(CC) -o $@  $(APPS-y) -Tapps/apps.ld -lfrosted -lc -lfrosted -Wl,-Map,apps.map  $(LDFLAGS) $(CFLAGS) $(EXTRA_CFLAGS)
 
@@ -33,6 +53,7 @@ libfrosted.a: libfrosted
 clean:
 	make -C apps clean
 	make -C libfrosted clean
+	@rm -f apps/apps.ld
 	@rm -f apps.elf
 	@rm -f apps.map
 
---
+++ libfrosted/string.c
@@ -0,0 +1,176 @@
+/*  
+ *      This file is part of frosted.
+ *
+ *      frosted is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License version 2, as 
+ *      published by the Free Software Foundation.
+ *      
+ *
+ *      frosted is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with frosted.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Authors: Daniele Lacamera, Maxime Vincent
+ *
+ */  
+
+
+/* 
+ * memset implementation
+ *
+ */
+
+#include <stddef.h>
+
+void * memset(void *s, int c, size_t n)
+{
+	unsigned char *d = (unsigned char *)s;
+
+	while (n--) {
+		*d++ = (unsigned char)c;
+	}
+
+	return s;
+}
+/* 
+ * strcat implementation
+ *
+ */
+
+#include <string.h>
+
+
+char *strcat(char *dest, const char *src)
+{
+    int i = 0;
+    int j = strlen(dest);
+    for (i = 0; i < strlen(src); i++) {
+        dest[j++] = src[i];
+    }
+    dest[j] = '\0';
+    return dest;
+}
+/* 
+ * strcmp implementation
+ *
+ */
+
+#include <string.h>
+
+int strcmp(const char *s1, const char *s2)
+{
+    int diff = 0;
+    while(!diff && *s1)
+    {
+        diff = (int)*s1 - (int)*s2;
+        s1++;
+        s2++;
+    }
+	return diff;
+}
+/* 
+ * strcmp implementation
+ *
+ */
+
+#include <string.h>
+
+size_t strlen(const char *s)
+{
+    int i = 0;
+    while(s[i] != 0)
+        i++;
+    return i;
+}
+/* 
+ * strcat implementation
+ *
+ */
+
+#include <string.h>
+
+
+char *strncat(char *dest, const char *src, size_t n)
+{
+    int i = 0;
+    int j = strlen(dest);
+    for (i = 0; i < strlen(src); i++) {
+        if (j >= (n - 1)) {
+            break;
+        }
+        dest[j++] = src[i];
+    }
+    dest[j] = '\0';
+    return dest;
+}
+/* 
+ * strncmp implementation
+ *
+ */
+
+#include <string.h>
+
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+    int diff = 0;
+    while(!diff && *s1 && n)
+    {
+        diff = (int)*s1 - (int)*s2;
+        s1++;
+        s2++;
+        n--;
+    }
+	return diff;
+}
+
+/*
+ * memcpy implementation
+ *
+ */
+
+void *memcpy(void *dst, const void *src, size_t n)
+{
+    int i;
+    const char *s = (const char *)src;
+    char *d = (char *)dst;
+    for (i = 0; i < n; i++) {
+        d[i] = s[i];
+    }
+    return dst;
+}
+
+/* 
+ * strncpy implementation
+ *
+ */
+char *strncpy(char *dst, const char *src, size_t n)
+{
+    int i;
+    for (i = 0; i < n; i++) {
+        dst[i] = src[i];
+        if (src[i] == '\0')
+            break;
+    }
+    return dst;
+}
+
+/* 
+ * strcpy implementation
+ *
+ */
+char *strcpy(char *dst, const char *src)
+{
+    int i = 0;
+    while(1 < 2) {
+        dst[i] = src[i];
+        if (src[i] == '\0')
+            break;
+        i++;
+    }
+    return dst;
+}
+
